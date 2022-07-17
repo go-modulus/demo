Dragomeat/modulus-fx

Rename module to demo

internal/user/action/register.go
@@ -1,10 +1,10 @@
 package action
 
 import (
-	"boilerplate/internal/framework"
-	"boilerplate/internal/user/dto"
-	"boilerplate/internal/user/service"
 	"context"
+	"demo/internal/framework"
+	"demo/internal/user/dto"
+	"demo/internal/user/service"
 	application "github.com/debugger84/modulus-application"
 	"github.com/ggicci/httpin"
 	"github.com/go-chi/chi/v5"

internal/user/service/registration.go
@@ -1,9 +1,9 @@
 package service
 
 import (
-	"boilerplate/internal/user/dao"
-	"boilerplate/internal/user/dto"
 	"context"
+	"demo/internal/user/dao"
+	"demo/internal/user/dto"
 	application "github.com/debugger84/modulus-application"
 	"github.com/gofrs/uuid"
 	"time"

go-modulus/demo

internal/user/action/register.go
@@ -1,10 +1,10 @@
 package action
 
 import (
-	"boilerplate/internal/framework"
-	"boilerplate/internal/user/service"
-	"boilerplate/internal/user/storage"
 	"context"
+	"demo/internal/framework"
+	"demo/internal/user/service"
+	"demo/internal/user/storage"
 	application "github.com/debugger84/modulus-application"
 	"github.com/ggicci/httpin"
 	"github.com/go-chi/chi/v5"

internal/user/service/registration.go
@@ -1,10 +1,10 @@
 package service
 
 import (
-	"boilerplate/internal/framework"
-	"boilerplate/internal/user/dao"
-	"boilerplate/internal/user/storage"
 	"context"
+	"demo/internal/framework"
+	"demo/internal/user/dao"
+	"demo/internal/user/storage"
 	application "github.com/debugger84/modulus-application"
 	"github.com/gofrs/uuid"
 	guid "github.com/google/uuid"